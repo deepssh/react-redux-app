ffd744eb65374c3a06ee366c5ec7b1190395e2bb
Deepesh Krishnan <deepesh.k@infogain.com>
2017-07-19 12:07:02 +0530

    first commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+
+  if (!video){
+    return <div>Loading ...</div>
+  };
+
+  const videoid = video.id.videoid;
+  const url = `https://www.youtube.com/embed/${videoid}`;
+  return(
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details" >
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
---
+++ src/index.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+// const API_KEY = 'AIzaSyBOTE2H0l67ZHI7SMy9FAxwtB2IjBdJb_M';
+const API_KEY = config.API_KEY;
+
+
+//create a new component that produces some html
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: {term}}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]})
+      });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
+    return (
+      <div>
+        {/* // term => this.videoSearch(term) */}
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+// take this html component and put this on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
